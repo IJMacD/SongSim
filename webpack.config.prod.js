ae02afdc06087138cf816a4d5db75cda4c253d41
Iain MacDonald <IJMacD@gmail.com>
2017-10-16 00:15:47 +0800

    Production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,18 +1,21 @@
+const path = require('path');
 const webpack = require('webpack');
 const config = require('./webpack.config');
 
-config.entry = "src/index.js"
+config.entry = "./src/index.js"
 config.output.path = path.resolve('dist');
 config.output.filename = "[name].min.js";
 config.output.publicPath = "";
 config.devtool = "source-map";
-config.plugins.push(new webpack.optimize.CommonsChunkPlugin({
-    name: "vendor",
-    minChunks (module) {
-        return module.context && module.context.includes('node_modules');
-    },
-}));
-config.plugins.push(new webpack.optimize.UglifyJsPlugin());
-config.plugins.push(new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"' }}));
+config.plugins = [
+    new webpack.optimize.CommonsChunkPlugin({
+        name: "vendor",
+        minChunks (module) {
+            return module.context && module.context.includes('node_modules');
+        },
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"' }}),
+];
 
 module.exports = config;
